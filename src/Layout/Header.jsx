c7bebbcad38d28499aadbfff7752ae592063c8d2
Saurabh-yadav07 <syadav9718910607@gmail.com>
2025-11-30 12:14:31 +0530

    first commit made header component

---
+++ src/Layout/Header.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import HeaderImg from "../assets/cuisines.jpg"
+const Header=()=>{
+return (
+    <>
+    <header>
+        <h1>Cuisines by Srv</h1>
+        <div>
+            <img src="Headerimg"></img>
+        </div>
+    </header>
+    </>
+)
+}
+export default Header;
